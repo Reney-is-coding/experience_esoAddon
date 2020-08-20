baf890df3210a67b7fed52b52445f2ef69161cb5
antilope <benjamin.grout@epitech.eu>
2020-08-20 16:05:35 +0200

    echo exp

--- Addon.lua
+++ Addon.lua
@@ -1,4 +1,5 @@
 local counter = 1
+local currentXP = GetUnitXP('player')
 
 function AddonUpdate()
     AddonCounter:SetText(string.format("Counter: %d", counter))
@@ -7,4 +8,14 @@ end
 
 function AddOnReset()
     counter = 0
+
+function Addon_OnInitialized(self)
+  self:RegisterForEvent(EVENT_EXPERIENCE_UPDATE, OnXPUpdate)
+  EVENT_MANAGER:RegisterForEvent("MyAddon", EVENT_EXPERIENCE_UPDATE, OnXPUpdate)
+
+function OnXPUpdate( eventCode, unitTag, currentExp, maxExp, reason )
+       if ( unitTag ~= 'player' ) then return end
+       local XPgain = currentExp - MyAddon.currentXP
+       d("You gained " .. XPgain .. " experience.")
+       MyAddon.currentXP = currentExp
 end
